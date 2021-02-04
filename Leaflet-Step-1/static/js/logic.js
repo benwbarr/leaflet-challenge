e0dc4b4e885f56dee7d59ff3a9815c74ec78ad82
Benjamin Barr <benwbarr@gmail.com>
2021-02-04 11:11:56 -0700

    fixed logic

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -13,7 +13,6 @@ function createMap(earthquakes) {
     // Define lightmap and darkmap
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
         maxZoom: 20,
         id: "mapbox/light-v10",
         accessToken: API_KEY
